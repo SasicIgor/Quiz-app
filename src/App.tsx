7916ff36071ddfe9b306fc5dc928eb8c647b6021
Igor-Sasic <igor.sasic811@gmail.com>
2025-10-21 18:20:08 +0200

    quiz functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { useQuizContext } from "./store/quiz-context/useQuizContext";
 import QuizSetup from "./components/QuizSetup";
 import Quiz from "./components/Quiz";
+import { useQTrackerContext } from "./store/quiz-tracker-context/useQTrackerContext";
 
 function App() {
   const { state } = useQuizContext();
+  const { state: trakerState } = useQTrackerContext();
   return (
     <>
       <h1 className="text-3xl font-bold"></h1>
       {state.status === "inactive" && <QuizSetup />}
       {state.status === "active" && <Quiz />}
-      <p>{state.status}</p>
+      {state.status === "finished" && trakerState.correctAnswers}
     </>
   );
 }
